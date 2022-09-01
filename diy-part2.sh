ea9e070055b3b8bc3eb38f80f931b4365b4089c1
3652365 <desmond.beyond@gmail.com>
2022-09-01 14:54:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,15 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.123/g' package/base-files/files/bin/config_generate
+mkdir package/tt/
+# git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+# git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/openwrt-smartdns
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/tt/openwrt-passwall
+git clone https://github.com/sirpdboy/luci-app-netdata.git package/tt/luci-app-netdata
+git clone https://github.com/netdata/netdata.git package/tt/netdata
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/tt/luci-app-adguardhome
+# git clone https://github.com/AdguardTeam/AdGuardHome.git package/tt/AdGuardHome
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/tt/luci-app-unblockneteasemusic
+#git clone https://github.com/small-5/luci-app-adblock-plus.git package/tt/luci-app-adblock-plus
+#git clone https://github.com/kenzok8/small.git package/tt/small
+git clone https://github.com/fw876/helloworld.git package/tt/helloworld
